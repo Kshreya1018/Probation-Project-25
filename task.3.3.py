22f4c5d9eec5d4a25294c2ccef1d0dd43c1ef932
shreya sahu <shreya24164012@akgec.ac.in>
2025-09-20 13:43:40 +0530

    numpy basics

---
+++ task.3.3.py
@@ -0,0 +1,17 @@
+import numpy as np
+a=np.random.randint(1,20,size=(10))
+b=np.random.randint(1,20,size=(10))
+add=a+b
+sub=a-b
+mul=a*b
+div=a//b
+sq=a**2
+d=np.dot(a,b)
+sorted=np.sort(b)
+print(add)
+print(sub)
+print(mul)
+print(div)
+print(sq)
+print(d)
+print(sorted)
